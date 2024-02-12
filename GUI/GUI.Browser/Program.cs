7454799982dee6af986e2007e2b414bb4d6e5c5a
LokiSharp <loki.sharp@gmail.com>
2024-02-12 22:43:57 +0800

    feat: 初始化 Avalonia 跨平台 GUI

---
+++ GUI/GUI.Browser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Versioning;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Browser;
+using Avalonia.ReactiveUI;
+using GUI;
+
+[assembly: SupportedOSPlatform("browser")]
+
+internal sealed class Program
+{
+    private static Task Main(string[] args)
+    {
+        return BuildAvaloniaApp()
+            .WithInterFont()
+            .UseReactiveUI()
+            .StartBrowserAppAsync("out");
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>();
+    }
+}
