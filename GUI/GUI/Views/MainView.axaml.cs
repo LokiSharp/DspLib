5bf21e810fb7672ca73c29ea6130e5a833dac36e
LokiSharp <loki.sharp@gmail.com>
2024-02-13 00:05:12 +0800

    feat: 创建 DataBase GUI

--- GUI/GUI/Views/MainView.axaml.cs
+++ GUI/GUI/Views/MainView.axaml.cs
@@ -1,4 +1,8 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using DspLib.DataBase;
 
 namespace GUI.Views;
 
@@ -7,5 +11,31 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    public void InitButtonClicked(object source, RoutedEventArgs args)
+    {
+        Debug.WriteLine($"Click! {Host.Text} {DataBase.Text} {Username.Text} {Password.Text}");
+        InitDatabase(Host.Text!, DataBase.Text!, Username.Text!, Password.Text!);
+    }
+
+    public async void InsertButtonClicked(object source, RoutedEventArgs args)
+    {
+        await InsertDatabase(Host.Text!, DataBase.Text!, Username.Text!, Password.Text!,
+            int.Parse(StartSeed.Text!), int.Parse(MaxSeed.Text!), int.Parse(StarCount.Text!));
+    }
+
+    private void InitDatabase(string host, string database, string username, string password)
+    {
+        var connectionString = new ConnectionString(host, database, username, password).GetString();
+        new DatabaseInitializer(connectionString).CreateTable();
+    }
+
+    private async Task InsertDatabase(string host, string database, string username, string password, int startSeed,
+        int maxSeed,
+        int starCount)
+    {
+        var connectionString = new ConnectionString(host, database, username, password).GetString();
+        await new DatabaseInserter(connectionString).InsertGalaxiesInfoInBatch(startSeed, maxSeed, starCount);
     }
 }
